88ab64fb6b342b2b8c813349b16b4c29bfcf67ad
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-05-12 19:12:06 -0300

    Updates

---
+++ precache-manifest.df3322d2a7ccd8d228c125084e9881d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ff6a4cf53ebb09c803a3b4cba0e575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0281299956b31bff09",
+    "url": "/static/css/main.0fbe0525.chunk.css"
+  },
+  {
+    "revision": "fef21d49c6e6f4a6d0f7",
+    "url": "/static/js/2.3c9be6f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c9be6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0281299956b31bff09",
+    "url": "/static/js/main.986f0faf.chunk.js"
+  },
+  {
+    "revision": "e805c25b4204dbe8397c",
+    "url": "/static/js/runtime-main.c1f08748.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/static/media/logo.82a7c062.svg"
+  }
+]);
